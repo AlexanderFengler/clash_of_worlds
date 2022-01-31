6f6c3e99bd8166292cc37f7556a551c16fa91255
Alexander Fengler <alexanderfengler@gmx.de>
2022-01-31 09:49:07 -0600

    ..

--- run_model.py
+++ run_model.py
@@ -16,11 +16,11 @@ import yaml as yml
 from multiprocessing import Pool
 from functools import partial
 
-def run_subj_model_reg(df_subj, model_idx, reg_models = None,
-                       include = None,
-                       n_mcmc = 2000, n_burnin = 500,
-                       n_chains = 1, save_folder = None,
-                       started_by_mp = False):
+def run_subj_model(df_subj, model_idx, mode,
+                   n_mcmc = 2000, n_burnin = 500,
+                   include = ['z'],
+                   n_chains = 1, save_folder = None,
+                   started_by_mp = False):
     
     """This is the main function used for model fits. 
     
@@ -32,6 +32,8 @@ def run_subj_model_reg(df_subj, model_idx, reg_models = None,
             String that provides a name for the model fit. This is only used to generated an appropriate save file.
         reg_models: list of dictionaries <default=None>
             A list of dictionaries, where each dictionary holds a reg_model as expected by the HDDMRegressor class.
+        mode: str <default='navarro'>
+            Specifies whether to run the hddm via analytical likelihood ('navarro') or via LANs ('lan')
         include: list <default=None>
             A list of strings, which specify parameters to fit, which are not regressor outcomes (see HDDM documentation)
         n_mcmc: int <default=2000>
@@ -52,20 +54,27 @@ def run_subj_model_reg(df_subj, model_idx, reg_models = None,
                                         'but did not specify a folder to save the model in.'
         
     # Extract subject id from data (useful for naming of saved files etc.)
-    subj_idx = df_subj['subject_uuid'].values[0]
+    subj_idx = df_subj['subj_idx'].values[0]
     
     # ACTUAL RUN  ---------------------------------------------------
     for chain in range(1, n_chains + 1, 1):
         start_t = time()
-        model_ = hddm.HDDM(df_subj,
-                            include = 'z')
+        if mode == 'navarro':
+            model_ = hddm.HDDM(df_subj,
+                               include = include)
 
-        db_file_name = save_folder + '/db_chain_' + str(chain) + '_' + model_idx + '_subj_' + subj_idx + '.db'
-        model_file_name = save_folder + '/model_chain_' + str(chain) + '_' + model_idx + '_subj_' + subj_idx + '.pickle'
+        if mode == 'lan':
+            model_ = hddm.HDDMnn(df_subj,
+                                 model = 'ddm',
+                                 include = include)
+            
+        db_file_name = save_folder + '/' + mode + '_db_chain_' + str(chain) + '_' + model_idx + '_subj_' + str(subj_idx) + '.db'
+        model_file_name = save_folder + '/' + mode + '_model_chain_' + str(chain) + '_' + model_idx + '_subj_' + str(subj_idx) + '.pickle'
 
         # Check if model file name exists --> if yes don't run anything
         if os.path.exists(model_file_name):
             print(model_file_name, ' EXISTS --> NOT RUNNING THIS MODEL')
+            return 0
         else:
             try:
                 model_.sample(n_mcmc + n_burnin, n_burnin, 
@@ -86,6 +95,7 @@ def run_subj_model_reg(df_subj, model_idx, reg_models = None,
                                                  'dic', 
                                                  'traces', 
                                                  'time'])
+
             model_data = model_data.append({'model_idx': model_idx, 'subj_idx': subj_idx, 
                                'model_file': model_file_name, 'db_file': db_file_name,
                                'data': df_subj,
@@ -94,8 +104,8 @@ def run_subj_model_reg(df_subj, model_idx, reg_models = None,
                                'time': time() - start_t},
                                ignore_index = True)
 
-            data_file_name = save_folder + '/df_chain_' + str(chain) + '_' + model_idx + '_subj_' + subj_idx + '.pickle'
-            pickle.dump(model_data, open(data_file_name, 'wb')) 
+            data_file_name = save_folder + '/' + mode + '_df_chain_' + str(chain) + '_' + model_idx + '_subj_' + str(subj_idx) + '.pickle'
+            pickle.dump(model_data, open(data_file_name, 'wb'))
     
     if started_by_mp:
         return 1
@@ -118,6 +128,9 @@ if __name__ == "__main__":
     CLI.add_argument('--model_idx',
                      type = str,
                      default = None)
+    CLI.add_argument('--mode',
+                     type = str,
+                     default = None)
     CLI.add_argument('--n_mcmc', 
                      type = int,
                      default = 2000)
@@ -137,19 +150,8 @@ if __name__ == "__main__":
     # Raw data dict
     data = pickle.load(open(args.data, 'rb'))
     
-    # LOAD MODEL SPEC
-    model_spec = yml.load(open('model_specs.yml', 'rb'), Loader = yml.Loader)[args.model_idx]
-    
-    print('LOADED FROM MODEL SPEC: ', args.model_idx)
-    #print(model_spec)
-
     # Subjects to look at
     subj_ids = list(data.keys())
-    
-    if not len(model_spec['reg_models']) > 0:
-        print('You included no regressors! Make sure this is intended!')
-    else:
-        print('regressors: ', model_spec['reg_models'])
     
     # RUN MODELS ------------------------------------------------------------------
     if not args.parallel:
@@ -157,36 +159,33 @@ if __name__ == "__main__":
         for subj_idx in subj_ids:
             print(subj_idx)
             print('Model ' + str(cnt) + ' of ' + str(len(subj_ids)))
-            df_subj = data[subj_idx]['data']
+            df_subj = data[subj_idx]['dataset']
             print(df_subj)
 
-            model_tmp = run_subj_model_reg(df_subj, model_idx = args.model_idx, reg_models = model_spec['reg_models'],
-                                           include = model_spec['includes'],
-                                           n_mcmc = args.n_mcmc, n_burnin = args.n_burnin, n_chains = args.n_chains,
-                                           save_folder = args.save_folder,
-                                           started_by_mp = False)
+            model_tmp = run_subj_model(df_subj, model_idx = args.model_idx, mode = args.mode,
+                                       include = ['z'],
+                                       n_mcmc = args.n_mcmc, n_burnin = args.n_burnin, n_chains = args.n_chains,
+                                       save_folder = args.save_folder,
+                                       started_by_mp = False)
             cnt += 1
     else:
-        n_cpus = psutil.cpu_count(logical = False) - 1
+        n_cpus = psutil.cpu_count(logical = False) - 2
         print('N CPUS: ', n_cpus)
         
         # Prepare data to be passed to run_subj_model_reg)
-        par_args = [data[subj_idx]['data'] for subj_idx in subj_ids]
+        par_args = [data[subj_idx]['dataset'] for subj_idx in subj_ids]
         
-        if model_spec['includes'] == 'None':
-            model_spec['includes'] = ()
-        
-        prepped_run_subj_model_reg = partial(run_subj_model_reg, 
-                                             model_idx = args.model_idx,
-                                             reg_models = model_spec['reg_models'],
-                                             include = model_spec['includes'],
-                                             n_mcmc = args.n_mcmc, n_burnin = args.n_burnin,
-                                             n_chains = args.n_chains, save_folder = args.save_folder,
-                                             started_by_mp = True)
+        prepped_run_subj_model = partial(run_subj_model, 
+                                         model_idx = args.model_idx,
+                                         mode = args.mode,
+                                         include = ['z'],
+                                         n_mcmc = args.n_mcmc, n_burnin = args.n_burnin,
+                                         n_chains = args.n_chains, save_folder = args.save_folder,
+                                         started_by_mp = True)
         
         # Starting pool
         with Pool(processes = n_cpus) as pool:
-            pool_out = pool.map(prepped_run_subj_model_reg, par_args)
+            pool_out = pool.map(prepped_run_subj_model, par_args)
         
         print('POOL OUT: ', pool_out)
         if (-1) not in pool_out:
---
+++ run_models.sh
@@ -0,0 +1,73 @@
+# DDM
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_ddm.pickle \
+#                        --save_folder hddm_models/ddm/ \
+#                        --model_idx ddm \
+#                        --mode navarro \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_angle.pickle \
+#                        --save_folder hddm_models/angle/ \
+#                        --model_idx angle \
+#                        --mode navarro \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_weibull.pickle \
+#                        --save_folder hddm_models/weibull/ \
+#                        --model_idx weibull \
+#                        --mode navarro \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_ornstein.pickle \
+#                        --save_folder hddm_models/ornstein/ \
+#                        --model_idx ornstein \
+#                        --mode navarro \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# LAN
+python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_ddm.pickle \
+                       --save_folder hddm_models/ddm/ \
+                       --model_idx ddm \
+                       --mode lan \
+                       --n_mcmc 1500 \
+                       --n_burnin 500 \
+                       --n_chains 1 \
+                       --parallel 1
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_angle.pickle \
+#                        --save_folder hddm_models/angle/ \
+#                        --model_idx angle \
+#                        --mode lan \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_weibull.pickle \
+#                        --save_folder hddm_models/weibull/ \
+#                        --model_idx weibull \
+#                        --mode lan \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
+
+# python /Users/afengler/OneDrive/proj_clash_of_worlds/clash_of_worlds/run_model.py --data data/param_recov_dataset_ornstein.pickle \
+#                        --save_folder hddm_models/ornstein/ \
+#                        --model_idx ornstein \
+#                        --mode lan \
+#                        --n_mcmc 1500 \
+#                        --n_burnin 500 \
+#                        --n_chains 1 \
+#                        --parallel 0
