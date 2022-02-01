fbc16caab7e0700572db049144841588c3a8e753
Alexander Fengler <alexanderfengler@gmx.de>
2022-02-01 17:32:52 -0600

    ..

--- run_models.sh
+++ run_models.sh
@@ -6,20 +6,22 @@ n_chains=4
 parallel=1
 
 for model_idx in "${model_idxs[@]}"
-    do
-        for mode in "${mode[@]}"
-            do
-            data="data/parameter_recovery/param_recov_dataset_$model_idx.pickle"
-            save_folder="hddm_models/$model_idx/"
+do  
+    echo "passed"
+    for mode in "${mode[@]}"
+        echo "passed inner"
+        do
+        data="data/parameter_recovery/param_recov_dataset_$model_idx.pickle"
+        save_folder="hddm_models/$model_idx/"
 
-            # DDM
-            sbatch sbatch_run_models_hddm.sh --data $data \
-                                             --save_folder $save_folder \
-                                             --model_idx $model_idx \
-                                             --mode $mode \
-                                             --n_mcmc $n_mcmc \
-                                             --n_burnin $n_burn \
-                                             --n_chains $n_chains \
-                                             --parallel $parallel
-            done
-    done
+        # DDM
+        sbatch sbatch_run_models_hddm.sh --data $data \
+                                            --save_folder $save_folder \
+                                            --model_idx $model_idx \
+                                            --mode $mode \
+                                            --n_mcmc $n_mcmc \
+                                            --n_burnin $n_burn \
+                                            --n_chains $n_chains \
+                                            --parallel $parallel
+        done
+done
